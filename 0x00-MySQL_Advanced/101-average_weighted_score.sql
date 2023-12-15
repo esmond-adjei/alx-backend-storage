67f8a307e0b208e2859a76045e9b28f36ce9f326
esmond-adjei <xmongram@gmail.com>
2023-12-15 03:09:21 +0000

    modified some files

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -4,31 +4,25 @@ DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-  DECLARE total_weighted_score FLOAT(10, 6);
-  DECLARE total_weight FLOAT(10, 6);
-  DECLARE total_weighted_score_sum FLOAT(10, 6);
-  DECLARE total_weight_sum FLOAT(10, 6);
-  DECLARE user_id INT;
-  DECLARE project_id INT;
-  DECLARE project_weight INT;
-  DECLARE project_score FLOAT(10, 6);
-  DECLARE cur CURSOR FOR SELECT user_id, project_id, weight, score FROM corrections JOIN projects ON corrections.project_id = projects.id;
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET @done = TRUE;
-
-  SET total_weighted_score_sum = 0;
-  SET total_weight_sum = 0;
+  DECLARE user_id_var INT;
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE cur CURSOR FOR SELECT id FROM users;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
   OPEN cur;
-  FETCH cur INTO user_id, project_id, project_weight, project_score;
-  WHILE NOT @done DO
-    SET total_weighted_score = total_weighted_score + (project_weight * project_score);
-    SET total_weight = total_weight + project_weight;
-    FETCH cur INTO user_id, project_id, project_weight, project_score;
-  END WHILE;
-  CLOSE cur;
 
-  SELECT AVG(total_weighted_score / total_weight) INTO total_weighted_score_sum FROM users;
-  UPDATE users SET average_score = total_weighted_score_sum;
+  read_loop: LOOP
+    FETCH cur INTO user_id_var;
+
+    IF done THEN
+      LEAVE read_loop;
+    END IF;
+
+    CALL ComputeAverageWeightedScoreForUser(user_id_var);
+  END LOOP;
+
+  CLOSE cur;
 END //
 
 DELIMITER ;
+
