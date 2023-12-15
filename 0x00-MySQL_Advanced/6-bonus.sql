757bb4b562b48c26d793039ec8c6094f878ec799
esmond-adjei <xmongram@gmail.com>
2023-12-15 01:08:23 +0000

    modified some files

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,29 @@
+-- creates a stored procedure `AddBonus`
+-- that adds a new correction for a student.
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(
+  IN user_id INT,
+  IN project_name VARCHAR(255),
+  IN score INT
+)
+BEGIN
+  DECLARE project_id INT;
+
+  -- check if project exists
+  SELECT id
+  INTO project_id
+  FROM projects
+  WHERE name = project_name;
+
+  -- else create project
+  IF project_id is NULL THEN
+    INSERT INTO projects (name) VALUES (project_name);
+    SET project_id = LAST_INSERT_ID();
+  END IF;
+
+  INSERT INTO corrections (user_id, project_id, score)
+  VALUES (user_id, project_id, score);
+END //
+
+DELIMITER ;
