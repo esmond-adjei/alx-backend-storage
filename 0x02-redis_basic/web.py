5930137f25b3a424d89488a233c9f103b835fa26
esmond-adjei <xmongram@gmail.com>
2023-12-21 04:34:26 +0000

    modified some files

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,25 +8,22 @@ from functools import wraps
 from typing import Callable
 
 
+redis_store = redis.Redis()
+
+
 def cacher(method: Callable) -> Callable:
     """decorator for caching"""
     @wraps(method)
     def wrapper(url) -> str:
-        """caches output"""
-        local_redis_store = redis.Redis()  # Initialize local Redis store
-        local_redis_store.incr(f"count:{url}")
-        result = local_redis_store.get(f"result:{url}")
-        if result:
+        """caches ouput"""
+        redis_store.incr(f"count:{url}")
+        cached_result = redis_store.get(f"result:{url}")
+        if cached_result:
             return result.decode('utf-8')
-
-        try:
-            result = method(url)
-            local_redis_store.set(f"count:{url}", 0)
-            local_redis_store.setex(f"result:{url}", 10, result)
-            return result
-        except Exception as e:
-            print(f"Error fetching URL {url}: {e}")
-            return ""  # or handle the error according to your requirements
+        result = method(url)
+        redis_store.set(f"count:{url}", 0)
+        redis_store.setex(f"result:{url}", 10, result)
+        return result
 
     return wrapper
 
@@ -34,17 +31,4 @@ def cacher(method: Callable) -> Callable:
 @cacher
 def get_page(url: str) -> str:
     """makes a request to url and caches result"""
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Check for HTTP errors
-        return response.text
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching URL {url}: {e}")
-        raise  # Re-raise the exception for the decorator to handle
-
-
-# Example usage
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    content = get_page(url)
-    print(content)
+    return requests.get(url).text
