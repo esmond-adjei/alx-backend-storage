a1587df6554b6073a3f476ee6d550cab23a31c7f
esmond-adjei <xmongram@gmail.com>
2023-12-21 02:52:47 +0000

    modified some files

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,8 @@ import redis
 import uuid
 from typing import Union
 
+UnionMixin = Union[str, bytes, int, float]
+
 
 class Cache:
     """A cache class for storing redis data"""
@@ -13,22 +15,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: UnionMixin) -> str:
         """stores `data` in redis storage"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
-    def get(self, key: str, fn: Callable = None) \
-            -> Union[str, bytes, int, float]:
-        """gets data from redis storage"""
-        data = self._redis.get(key)
-        return fn(data) if fn is not None else data
-
-    def get_str(self, key: str) -> str:
-        """get a string from redis storage"""
-        return self.get(key, fn=lambda d: d.decode('utf-8'))
-
-    def get_int(self, key: str) -> int:
-        """get an integer from redis storage"""
-        return self.get(key, fn=int)
