69b53d323563b79926a6369d909eccb8024e0911
esmond-adjei <xmongram@gmail.com>
2023-12-21 02:56:02 +0000

    modified some files

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Redis Basics
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 UnionMixin = Union[str, bytes, int, float]
 
@@ -20,3 +20,16 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> UnionMixin:
+        """gets data from redis storage"""
+        data = self._redis.get(key)
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        """get a string from redis storage"""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """get an integer from redis storage"""
+        return self.get(key, fn=int)
